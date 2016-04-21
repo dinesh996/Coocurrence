cc624641638e06229236af26fe8a5849de2c0a7f
dinesh996 <dinkumar2707@gmail.com>
2016-04-21 21:20:37 +0530

    Steps to take.

---
+++ Steps.py
@@ -0,0 +1,8 @@
+# Make a grap with nodes as elements in keys_flat list and edges with 0 value.   : Complete
+# Make Co-occurence matrix for the tags. : Complete
+# Make Marginal counts Matrix. : Complete
+# Make Total Counts.   : Complete
+# Iterative Denoising.   :
+# Calculating Local Node Centrality (LNC).  :
+# Calculating Coherence.   :
+# Finding SMC: Grow-Shrink Algorithm.   :
